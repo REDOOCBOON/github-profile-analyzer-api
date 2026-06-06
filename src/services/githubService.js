103baa8c47585fd4a8d3cdc0358777433b9be200
Purrfect Developer <test@example.com>
2026-06-06 21:51:34 +0530

    Add GitHub token authentication

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -2,32 +2,25 @@ const axios = require("axios");
 
 const headers = {
   Accept: "application/vnd.github+json",
-  "User-Agent": "github-profile-analyzer"
+  "User-Agent": "github-profile-analyzer",
+  Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
 };
 
 async function getGithubData(username) {
-  try {
-    const profileRes = await axios.get(
-      `https://api.github.com/users/${username}`,
-      { headers }
-    );
+  const profileRes = await axios.get(
+    `https://api.github.com/users/${username}`,
+    { headers }
+  );
 
-    const repoRes = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=100`,
-      { headers }
-    );
+  const repoRes = await axios.get(
+    `https://api.github.com/users/${username}/repos?per_page=100`,
+    { headers }
+  );
 
-    return {
-      profile: profileRes.data,
-      repos: repoRes.data
-    };
-  } catch (error) {
-    console.log("GitHub Error:");
-    console.log(error.response?.status);
-    console.log(error.response?.data);
-
-    throw error;
-  }
+  return {
+    profile: profileRes.data,
+    repos: repoRes.data
+  };
 }
 
 module.exports = {
