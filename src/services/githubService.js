f2c05be1fbf09557a8dd0baeba24897951b12654
Purrfect Developer <test@example.com>
2026-06-06 21:45:38 +0530

    Addd github error logging

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -6,20 +6,28 @@ const headers = {
 };
 
 async function getGithubData(username) {
-  const profileRes = await axios.get(
-    `https://api.github.com/users/${username}`,
-    { headers }
-  );
+  try {
+    const profileRes = await axios.get(
+      `https://api.github.com/users/${username}`,
+      { headers }
+    );
 
-  const repoRes = await axios.get(
-    `https://api.github.com/users/${username}/repos?per_page=100`,
-    { headers }
-  );
+    const repoRes = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=100`,
+      { headers }
+    );
 
-  return {
-    profile: profileRes.data,
-    repos: repoRes.data
-  };
+    return {
+      profile: profileRes.data,
+      repos: repoRes.data
+    };
+  } catch (error) {
+    console.log("GitHub Error:");
+    console.log(error.response?.status);
+    console.log(error.response?.data);
+
+    throw error;
+  }
 }
 
 module.exports = {
